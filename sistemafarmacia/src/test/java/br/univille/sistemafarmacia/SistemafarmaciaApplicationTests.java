d44202ec311c6964a2701f72852ca93410b2790c
guilhermesibovicz <guilhermesibovicz2017@gmail.com>
2022-11-10 14:19:44 -0300

    first commit

---
+++ sistemafarmacia/src/test/java/br/univille/sistemafarmacia/SistemafarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.sistemafarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemafarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
