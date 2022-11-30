fc95ff2f6daaf4d1656409b640e0fa03ec09526c
guilhermesibovicz <guilhermesibovicz2017@gmail.com>
2022-11-30 16:23:37 -0300

    WIP: Tela de login

---
+++ src/main/java/br/univille/sistemafarmacia/repository/LoginRepository.java
@@ -0,0 +1,11 @@
+// package br.univille.sistemafarmacia.repository;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+
+// import br.univille.sistemafarmacia.entity.Login;
+
+// @Repository
+// public interface LoginRepository extends JpaRepository<Login,Long>{
+//     Login findByNome(String nome);
+// }
